f1bb3a978d16044d7e9b6ab50f2dbc73fcd2888b
Akshay Ashok <akshayashok@ufl.edu>
2023-09-17 04:41:13 -0400

    Styling changes

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, FormEvent } from 'react';
+import React, { useEffect, useState, FormEvent, useRef } from 'react';
 import { useRouter } from 'next/router';
 
 const Home: React.FC = () => {
@@ -7,16 +7,32 @@ const Home: React.FC = () => {
   var valid = false;
   var company = "";
   const router = useRouter();
-  const [isHovered, setIsHovered] = useState(false);
+
+  const [isAboutOpen, setIsAboutOpen] = useState(false);
+  const [isAuthorsOpen, setIsAuthorsOpen] = useState(false);
+
+  // const aboutDropdownRef = useRef<HTMLDivElement>(null);
+  // const authorsDropdownRef = useRef<HTMLDivElement>(null);
 
   // useEffect(() => {
-  //   fetch('http://localhost:8080/api/home')
-  //     .then(response => response.json())
-  //     .then((data) => { 
-  //       setMessage(data.message);
-  //       setPeople(data.people);
-  //     });
-  // }, []);
+  //   const handleClickOutside = (event: MouseEvent) => {
+  //     if (
+  //       aboutDropdownRef.current &&
+  //       !aboutDropdownRef.current.contains(event.target as Node) &&
+  //       isAboutDropdownOpen
+  //     ) {
+  //       setIsAboutDropdownOpen(false);
+  //     }
+
+  //     if (
+  //       authorsDropdownRef.current &&
+  //       !authorsDropdownRef.current.contains(event.target as Node) &&
+  //       isAuthorsDropdownOpen
+  //     ) {
+  //       setIsAuthorsDropdownOpen(false);
+  //     }
+  //   };
+  // });
 
   const handleSearch = async (e: FormEvent)=> {
     console.log('handleSearch called')
@@ -49,34 +65,90 @@ const Home: React.FC = () => {
     else {
       alert('Company not found in the S&P 500. Please try another stock.')
     }
-
-    // if(response.inSP500) {
-    //   // Redirect to result.tsx
-    //   router.push({
-    //     pathname: '/result',
-    //     query: { search }
-    //   });
-    // } else {
-    //   alert('Company not found in the S&P 500. Please try another stock.');
-    // }
   }
+
+  const toggleAbout = () => {
+    setIsAboutOpen(!isAboutOpen);
+    setIsAuthorsOpen(false); // Close the Authors section when opening About
+  }
+
+  const toggleAuthors = () => {
+    setIsAuthorsOpen(!isAuthorsOpen);
+    setIsAboutOpen(false); // Close the About section when opening Authors
+  }
+
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      const target = event.target as HTMLElement;
+      if (
+        (isAboutOpen || isAuthorsOpen) &&
+        target.closest('.navbar') === null &&
+        target.closest('.dropdown-buttons') === null
+      ) {
+        setIsAboutOpen(false);
+        setIsAuthorsOpen(false);
+      }
+    };
+
+    document.addEventListener('click', handleClickOutside);
+
+    return () => {
+      document.removeEventListener('click', handleClickOutside);
+    };
+  }, [isAboutOpen, isAuthorsOpen]);
 
   return (
     <div className='bg-gradient-navy flex flex-col items-center justify-center min-h-screen'>
-      <h1
-        className={`text-6xl font-extrabold mb-5 ${
-          isHovered ? 'animate-title' : ''
-        }`}
-        onMouseEnter={() => setIsHovered(true)}
-        onMouseLeave={() => setIsHovered(false)}
-        style={{
-          fontFamily: 'Arial, sans-serif', // Set your desired font family
-          color: 'white', // Set the text color
-          textShadow: '2px 2px 4px rgba(0, 0, 0, 0.5)', // Add a text shadow
-        }}
-      >
-        Stockstainable
-      </h1>
+      <div className="navbar w-full bg-gray-900 text-white p-4 fixed top-0 z-10">
+        <div className="flex justify-between items-center">
+          <div className="dropdown-buttons space-x-4">
+            <button
+              onClick={toggleAbout}
+              className="text-orange-300 hover:underline cursor-pointer"
+            >
+              About
+            </button>
+            {isAboutOpen && (
+              <div className="absolute mt-2 p-2 bg-white text-gray-900 rounded-lg shadow-md w-96">
+                Welcome to <b>StockStainable</b>!<br/>
+                We aim to provide our users with a tool to learn about and monitor the sustainability reputations and practices of S&P 500 companies. 
+                This tool computes a sustainability score for all aforementioned companies, 
+                derived from ESG &#40;Environmental, Social, and Business Governance&#41; scores,
+                sustainability news sentiment analysis using NLP, and other data
+                &#40;lawsuits, controversies, etc.&#41;.
+              </div>
+            )}
+            <button
+              onClick={toggleAuthors}
+              className="text-green-300 hover:underline cursor-pointer"
+            >
+              Authors
+            </button>
+            {isAuthorsOpen && (
+              <div className="absolute mt-2 p-2 bg-white text-gray-900 rounded-lg shadow-md">
+                <p>
+                  <a href="https://www.linkedin.com/in/nbutakow/" target="_blank" className='text-blue-500 underline'>
+                    Nic Butakow
+                  </a>
+                  <br />
+                  <a href="https://www.linkedin.com/in/alex-huang-954x/" target="_blank" className='text-blue-500 underline'>
+                    Alex Huang
+                  </a>
+                  <br />
+                  <a href="https://www.linkedin.com/in/akshayashok1/" target="_blank" className='text-blue-500 underline'>
+                    Akshay Ashok
+                  </a>
+                  <br />
+                  <a href="https://www.linkedin.com/in/dylan-mcgarry/" target="_blank" className='text-blue-500 underline'>
+                    Dylan McGarry
+                  </a>
+                </p>
+              </div>
+            )}
+          </div>
+        </div>
+      </div>
+      <img src='/stockstainable.png' className='mb-4' />
       <form onSubmit={handleSearch} className='flex items-center'>
         <input 
           type="text"
--- client/pages/result.tsx
+++ client/pages/result.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useRouter } from 'next/router';
 import Link from 'next/link';
 
@@ -13,15 +13,99 @@ const Result: React.FC = () => {
     ticker = search[1];
   }
 
-  
+  const [isAboutOpen, setIsAboutOpen] = useState(false);
+  const [isAuthorsOpen, setIsAuthorsOpen] = useState(false);
+
+  const toggleAbout = () => {
+    setIsAboutOpen(!isAboutOpen);
+    setIsAuthorsOpen(false); // Close the Authors section when opening About
+  }
+
+  const toggleAuthors = () => {
+    setIsAuthorsOpen(!isAuthorsOpen);
+    setIsAboutOpen(false); // Close the About section when opening Authors
+  }
+
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      const target = event.target as HTMLElement;
+      if (
+        (isAboutOpen || isAuthorsOpen) &&
+        target.closest('.navbar') === null &&
+        target.closest('.dropdown-buttons') === null
+      ) {
+        setIsAboutOpen(false);
+        setIsAuthorsOpen(false);
+      }
+    };
+
+    document.addEventListener('click', handleClickOutside);
+
+    return () => {
+      document.removeEventListener('click', handleClickOutside);
+    };
+  }, [isAboutOpen, isAuthorsOpen]);
 
   return (
-    <div className="bg-gradient-navy flex flex-col items-center justify-center min-h-screen">
-      <h1 className="text-3xl mb-4 font-bold text-white">Search Result</h1>
-      <p className="text-white mb-4">You searched for: {companyName} &#40;{ticker}&#41;</p>
-      <Link href="/">
-        <span className="text-blue-500 hover:underline cursor-pointer">Go back to search</span>
-      </Link>
+    <div className="bg-gradient-navy flex flex-col items-center justify-center min-h-screen relative">
+        <div className="navbar w-full bg-gray-900 text-white p-4 fixed top-0 z-10">
+            <div className="flex justify-between items-center">
+                <div className="dropdown-buttons space-x-4">
+                    <button className="text-blue-300 hover:underline cursor-pointer">
+                        <Link href="/">
+                            <span>Home</span>
+                        </Link>
+                    </button>
+                    <button
+                        onClick={toggleAbout}
+                        className="text-orange-300 hover:underline cursor-pointer"
+                        >
+                        About
+                    </button>
+                    {isAboutOpen && (
+                    <div className="absolute mt-2 p-2 bg-white text-gray-900 rounded-lg shadow-md w-96">
+                        Welcome to <b>StockStainable</b>!<br/>
+                        We are a sustainability-observing tool for S&P 500 companies. 
+                        This tool computes a sustainability score for all aforementioned companies, 
+                        derived from ESG &#40;Environmental, Social, and Business Governance&#41; scores,
+                        sustainability news sentiment analysis using NLP, and other data
+                        &#40;lawsuits, controversies, etc.&#41;.
+                    </div>
+                    )}
+                    <button
+                    onClick={toggleAuthors}
+                    className="text-green-300 hover:underline cursor-pointer"
+                    >
+                    Authors
+                    </button>
+                    {isAuthorsOpen && (
+                    <div className="absolute mt-2 p-2 bg-white text-gray-900 rounded-lg shadow-md">
+                        <p>
+                        <a href="https://www.linkedin.com/in/nbutakow/" target="_blank" className='text-blue-500 underline'>
+                            Nic Butakow
+                        </a>
+                        <br />
+                        <a href="https://www.linkedin.com/in/alex-huang-954x/" target="_blank" className='text-blue-500 underline'>
+                            Alex Huang
+                        </a>
+                        <br />
+                        <a href="https://www.linkedin.com/in/akshayashok1/" target="_blank" className='text-blue-500 underline'>
+                            Akshay Ashok
+                        </a>
+                        <br />
+                        <a href="https://www.linkedin.com/in/dylan-mcgarry/" target="_blank" className='text-blue-500 underline'>
+                            Dylan McGarry
+                        </a>
+                        </p>
+                    </div>
+                    )}
+                </div>
+            </div>
+        </div>
+        <div className="text-white absolute top-20 left-0 right-0 text-center">
+            <h1 className="text-3xl font-bold">{companyName} ({ticker})</h1>
+        </div>
+        <p className="text-white mb-4">*information*</p>
     </div>
   );
 };
